cb685644a61ca558b60edc9f89c6ab0d6f5cb5d1
dkateros <dkateros@gmail.com>
2021-12-24 13:25:24 +0200

    split stubs of contract and java first services to different modules, fixed README

---
+++ test-generated/src/main/java/gr/dkateros/ws/types/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.dkateros.gr/ws/types", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package gr.dkateros.ws.types;
